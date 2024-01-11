cf2bbe8c3eaead6ce52f582b8d0b43112a7269a3
Hyeonjeong <ghghjj652@naver.com>
2024-01-11 15:00:13 +0900

    test: return 실습

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const calculator = {
-  add: function (a, b) {
-    console.log(a + b);
+  plus: function (a, b) {
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
+  },
+  times: function (a, b) {
+    return a * b;
   },
   divide: function (a, b) {
-    console.log(a / b);
+    return a / b;
   },
-  powerof: function (a, b) {
-    console.log(a ** b);
+  power: function (a, b) {
+    return a ** b;
   },
 };
 
-calculator.add(5, 2);
-calculator.minus(5, 2);
-calculator.divide(5, 2);
-calculator.powerof(5, 2);
+const plusResult = calculator.plus(5, 2);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
