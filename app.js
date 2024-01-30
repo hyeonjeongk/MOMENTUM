fd27d89c587c4c74a8074fca77fcf0074c1c5ca5
Hyeonjeong <ghghjj652@naver.com>
2024-01-30 15:25:56 +0900

    test: preventDefault 실습

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(event);
@@ -8,4 +10,10 @@ function onLoginSubmit(event) {
   // console.log(username);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
