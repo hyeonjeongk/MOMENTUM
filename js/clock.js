e7dfa688140202382caf3409c09dd843eca9b21a
Hyeonjeong <ghghjj652@naver.com>
2024-02-02 16:54:58 +0900

    feat: clock 기능 구현

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock(); // 즉시 한번 실행
+setInterval(getClock, 1000); // 1초마다 한번씩 실행
