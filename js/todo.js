07288427f46a858f8154ff70057e60e897d4cddd
khj <ghghjj652@naver.com>
2024-02-06 14:24:45 +0900

    feat: To Do List 2 - 사용자가 입력한 내용 list로 출력(li 태그)

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,19 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#todo-form input") : 윗줄과 동일한 결과
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span); //span을 li 내부로
+  span.innerText = newTodo; //span에 text넣기(사용자가 입력한 newTodo값)
+  toDoList.appendChild(li); //toDoList에 li 넣기
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value; // 새로운 변수에 toDoInput의 value 복사
   toDoInput.value = "";
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
