dab7db41f9cc7ec8058d10a414d6a8332d76db30
Hyeonjeong <ghghjj652@naver.com>
2024-02-14 16:49:31 +0900

    feat: To Do List 4 - 화면 및 localStorage에서 항목삭제

--- js/todo.js
+++ js/todo.js
@@ -16,14 +16,17 @@ function deleteToDo(event) {
   // console.dir(event.target.parentElement.innerText);
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos(); // local storage 갱신
 }
 
 function paintToDo(newTodo) {
-  // to do list 출력 함수
+  // to do list 출력 함수 : argument로 object 받음
   const li = document.createElement("li");
+  li.id = newTodo.id;
 
   const span = document.createElement("span");
-  span.innerText = newTodo; // span의 text변경(사용자가 입력한 newTodo값)
+  span.innerText = newTodo.text; // span의 text변경(사용자가 입력한 newTodo값)
 
   const button = document.createElement("button");
   button.innerText = "❌"; // button의 text변경
@@ -39,8 +42,13 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value; // 새로운 변수에 toDoInput의 value 복사
   toDoInput.value = ""; // input창 비우기
-  toDos.push(newTodo); // toDos array에 항목들 넣기
-  paintToDo(newTodo);
+  const newTodoObj = {
+    // to do를 object 형식으로 만들기
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj); // toDos array에 항목들 넣기
+  paintToDo(newTodoObj);
   saveToDos(); // toDos array를 local storage에 저장
 }
 
@@ -51,9 +59,15 @@ function sayHello(item) {
 }
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
-console.log(savedToDos);
+// console.log(savedToDos);
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos; // localStorage에 저장된 todo가 있으면 이전값 복원
   parsedToDos.forEach(paintToDo);
 }
+
+// function sexyFilter(todo) {
+//   // 해당 item을 새 array에 포함시키고 싶으면 true 리턴,
+//   // 아니면 false 리턴
+//   return todo.id !==
+// }
